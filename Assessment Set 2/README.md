5a77bfa2dd6f7949d9928bc1170e635f7265755b
Developers@Work <DevelopersWork@users.noreply.github.com>
2023-02-04 14:05:59 +0530

    Assessment Set 2: Code + documentation

--- Assessment Set 2/README.md
+++ Assessment Set 2/README.md
@@ -1,8 +1,33 @@
 # Assessment Set 2
 
+Simple HTTP server, which replies to queries related to calender.
+
 ## Commands
 
 ### Install
+
+Required packages are provided in **package.json**. To install execute below command
+
 `> npm install`
+
 ### Running
+
+The server is default to run on port **80**, this can be updated in the **index.js** script. Run the below command to start the server
+
 `> npm start`
+
+## Usage
+
+### API
+
+> wget --no-check-certificate --quiet
+> --method GET
+> --timeout=0
+> --header ''
+> 'http://localhost/command?q=subtract, 187 days from 2019-01-01 return the date'
+
+### Browser
+
+Open the following URL in the browser to query.
+
+`https://localhost`
--- Assessment Set 2/index.js
+++ Assessment Set 2/index.js
@@ -4,82 +4,130 @@ const express = require('express');
 const app = express();
 
 // setting port to serve the http
-const port = 443;
+const port = 80;
 
-// handles GET request on '/add' endpoint
-app.get('/add', (request, response) => {
-	const query = request.query;
-
-	// defining required and optional query params
-	const required = {date: new Date().toISOString()}
-	const optional = {days: '0', months: '0', years: '0'}
-
-	for(let key in required){
-		if(!query[key])
-			return response.status(500).json({
-				code: 500,
-				message: 'Incorrect Parameters',
-				_ts: new Date()
-			})
-		required[key] = query[key]
-	}
-
-	for(let key in optional){
-		if(query[key])
-			optional[key] = query[key]
-	}
-
-	const date = new Date(required['date']);
-	// setting day, month and year from the input provided
-	date.setDate(date.getDate() + parseInt(optional['days']));
-	date.setMonth(date.getMonth() + parseInt(optional['months']));
-	date.setFullYear(date.getFullYear() + parseInt(optional['years']));
-
-	// send response back to the client in the json format
-	response.status(200).json({
-		code: 200,
-		message: date.toISOString().split('T')[0],
-		_ts: new Date()
+// Function to return the response of incorrect query params
+function incorrectQueryParams(response, msg) {
+	return response.status(400).json({
+		status: 400,
+		code: 'Bad Request',
+		message: msg,
+		_ts: new Date(),
 	});
-});
+}
 
-// handles GET request on '/subtract' endpoint
-app.get('/subtract', (request, response) => {
+// handles GET request on '/command' endpoint
+app.get('/command', async (request, response) => {
 	const query = request.query;
 
-	const required = {date: new Date().toISOString()}
-	const optional = {days: '0', months: '0', years: '0'}
+	const required = { q: '' };
 
-	for(let key in required){
-		if(!query[key])
-			return response.status(500).json({
-				code: 500,
-				message: 'Incorrect Parameters',
-				_ts: new Date()
-			})
-		required[key] = query[key]
+	for (let key in required) {
+		if (!query[key])
+			return incorrectQueryParams(
+				response,
+				'Error: Required parameters missing in the API request.'
+			);
+		required[key] = query[key];
 	}
 
-	for(let key in optional){
-		if(query[key])
-			optional[key] = query[key]
-	}
+	const q = required['q'].toLowerCase().trim();
+	console.log(`${new Date().toISOString()}::${q}`);
+	let buffer = [];
 
-	const date = new Date(required['date']);
+	// Parsing the operation > add, subtract
+	buffer = q.split(',');
+	if (buffer.length < 2)
+		return incorrectQueryParams(
+			response,
+			'Error: Required parameters missing in the API request.'
+		);
+	const operation = buffer[0].trim();
+	if (operation != 'add' && operation != 'subtract')
+		return incorrectQueryParams(
+			response,
+			'"Error: Incorrect operation specified in the text-based query for date modification."'
+		);
 
-	date.setDate(date.getDate() - parseInt(optional['days']));
-	date.setMonth(date.getMonth() - parseInt(optional['months']));
-	date.setFullYear(date.getFullYear() - parseInt(optional['years']));
+	// Parsing date value, expected format yyyy-MM-dd
+	buffer = q.split('return');
+	if (buffer.length < 2)
+		return incorrectQueryParams(
+			response,
+			'Error: Required parameters missing in the API request.'
+		);
+	buffer = buffer[0].trim().split(' ');
+	if (buffer.length < 2)
+		return incorrectQueryParams(
+			response,
+			'Error: Required parameters missing in the API request.'
+		);
+	let date = buffer.splice(-1)[0].trim();
+	if (
+		date.length !== 10 ||
+		date.split('-').length !== 3 ||
+		date.split('-')[0].length !== 4 ||
+		date.split('-')[1].length !== 2 ||
+		date.split('-')[2].length !== 2
+	)
+		return incorrectQueryParams(
+			response,
+			'"Error: Incorrect Date specified in the text-based query for date modification."'
+		);
+	date = new Date(date);
 
-	response.status(200).json({
+	// parsing the value, expected an integer
+	buffer = buffer.join(' ').split(',')[1].trim().split(' ');
+	if (buffer.length < 2)
+		return incorrectQueryParams(
+			response,
+			'Error: Required parameters missing in the API request.'
+		);
+	let value = buffer[0];
+	if (!value.match('^-?[0-9]+$'))
+		return incorrectQueryParams(
+			response,
+			'"Error: Incorrect value specified in the text-based query for date modification."'
+		);
+	value = parseInt(value);
+
+	// parsing type, expected days, weeks, months and years only
+	const type = buffer[1];
+	if (type === 'days')
+		date.setDate(
+			operation === 'add' ? date.getDate() + value : date.getDate() - value
+		);
+	else if (type === 'weeks') {
+		value = value * 7;
+		date.setDate(
+			operation === 'add' ? date.getDate() + value : date.getDate() - value
+		);
+	} else if (type === 'months')
+		date.setMonth(
+			operation === 'add' ? date.getMonth() + value : date.getMonth() - value
+		);
+	else if (type === 'years')
+		date.setFullYear(
+			operation === 'add'
+				? date.getFullYear() + value
+				: date.getFullYear() - value
+		);
+	else
+		return incorrectQueryParams(
+			response,
+			'"Error: Incorrect type specified in the text-based query for date modification."'
+		);
+
+	// send the response
+	return response.status(200).json({
 		code: 200,
 		message: date.toISOString().split('T')[0],
-		_ts: new Date()
+		_ts: new Date(),
 	});
 });
 
 // Starting the http server, listens to the provided port
-app.listen(port, () => {
+app.listen(port, async () => {
 	console.log(`App started on Port: ${port}`);
 
 	// Enabling static files to be streamed
